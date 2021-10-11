1f0af0ac881c82b9281727a0a46e48493b1800ab
js25s <petrauskas.justas@gmail.com>
2021-10-11 10:38:00 +0200

    las thing

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./db/db-init.js";
+
+const server = express();
+
+const { PORT = 5000 } = process.env;
+
+server.use(cors());
+
+server.use(express.json());
+
+server.listen(PORT, async () => {
+  await connectDB();
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+server.on("error", (error) => {
+  console.log("Server is stoppped ", error);
+});
