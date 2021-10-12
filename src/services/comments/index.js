929c65cc30dd802be6c116c001f1d29ddcdb48e6
Istvan <steven.ford.j@gmail.com>
2021-10-12 10:50:08 +0300

    doing

---
+++ src/services/comments/index.js
@@ -0,0 +1,99 @@
+import express from "express";
+import models from "../../modules/relationTable/relations.js";
+import createHttpError from "http-errors";
+
+const router = express.Router();
+const { Comment, Post } = models;
+
+router
+  .route("/:postId")
+  .get(async (req, res, next) => {
+    try {
+      const targetPost = await Post.findByPk(req.params.postId);
+      if (targetPost) {
+        const comments = await Comment.findAll({
+          where: { post_id: req.params.postId },
+        });
+        res.send(comments);
+      } else
+        next(
+          createHttpError(404, `Post  with id ${req.params.postId} not found!`)
+        );
+    } catch (error) {
+      next(error);
+    }
+  })
+  .post(async (req, res, next) => {
+    try {
+      const targetPost = await Post.findByPk(req.params.postId);
+      if (targetPost) {
+        const newComments = await Comment.create({
+          ...req.body,
+          post_id: req.params.postId,
+        });
+        res.send(newComments);
+      } else
+        next(
+          createHttpError(404, `Post  with id ${req.params.postId} not found!`)
+        );
+    } catch (error) {
+      next(error);
+    }
+  });
+router
+  .route("/:postId/:commentId")
+  .put(async (req, res, next) => {
+    try {
+      const targetPost = await Post.findByPk(req.params.postId);
+      if (targetPost) {
+        const targetComment = await Post.findByPk(req.params.commentId);
+        if (targetComment) {
+          const updatedComment = await Comment.update(req.body, {
+            where: { id: req.params.commentId },
+            returning: true,
+          });
+          res.send(updatedComment);
+        } else {
+          next(
+            createHttpError(
+              404,
+              `Comment  with id ${req.params.commentId} not found!`
+            )
+          );
+        }
+      } else
+        next(
+          createHttpError(404, `Post  with id ${req.params.postId} not found!`)
+        );
+    } catch (error) {
+      next(error);
+    }
+  })
+  .delete(async (req, res, next) => {
+    try {
+      const targetPost = await Post.findByPk(req.params.postId);
+      if (targetPost) {
+        const targetComment = await Post.findByPk(req.params.commentId);
+        if (targetComment) {
+          const updatedComment = await Comment.destroy({
+            where: { id: req.params.commentId },
+          });
+          res.send(updatedComment);
+        } else {
+          next(
+            createHttpError(
+              404,
+              `Comment  with id ${req.params.commentId} not found!`
+            )
+          );
+        }
+      } else
+        next(
+          createHttpError(404, `Post  with id ${req.params.postId} not found!`)
+        );
+    } catch (error) {
+      next(error);
+    }
+  });
+
+export default router;
---
+++ src/services/posts/index.js
@@ -0,0 +1,102 @@
+import express from "express";
+import models from "../../modules/relationTable/relations.js";
+
+const postsRouter = express.Router();
+const { Post, Profile } = models;
+
+postsRouter
+  .route("/")
+  .get(async (req, res, next) => {
+    try {
+      const data = await Post.findAll({
+        include: [Profile],
+        limit: req.query.limit * 6 || 6,
+      });
+      res.send(data);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  })
+
+  .post(async (req, res, next) => {
+    try {
+      const post = await Post.create(req.body);
+      res.send(post);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  });
+
+postsRouter
+  .route("/:postId")
+
+  .get(async (req, res, next) => {
+    try {
+      const data = await Post.findOne({
+        where: { id: req.params.id },
+        include: Profile,
+      });
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send("Not found");
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  })
+
+  .put(async (req, res, next) => {
+    try {
+      const data = await Post.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+        returning: true,
+      });
+      res.send(data[1][0]);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  })
+
+  .delete(async (req, res, next) => {
+    try {
+      const rows = await Post.destroy({ where: { id: req.params.id } });
+      if (rows > 0) {
+        res.send("ok");
+      } else {
+        res.status(404).send("Not found");
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  })
+
+  .post(async (req, res, next) => {
+    try {
+      const path = req.file.path;
+      console.log(path);
+      const newImage = await Post.update(
+        { image: path },
+        {
+          where: {
+            id: req.params.postId,
+          },
+
+          returning: true,
+        }
+      );
+      res.send(newImage[1][0]);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  });
+
+export default postsRouter;
