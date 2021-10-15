09e4d863d03da77524174324ae40f51643387327
js25s <petrauskas.justas@gmail.com>
2021-10-15 10:07:28 +0200

    testing

--- src/modules/db-inits.js
+++ src/modules/db-inits.js
@@ -15,16 +15,16 @@ const sequelize = new Sequelize(DATABASE_URL, PGUSER, PGPASSWORD, {
   dialect: "postgres",
   // protocol: "postgres",
   // dialectOptions: {
-  //     ssl: {
-  //         require: false,
-  //         rejectUnauthorized: false
-  //     }
-  // }
+  //   ssl: {
+  //     require: false,
+  //     rejectUnauthorized: false,
+  //   },
+  // },
 });
 
 export const testDB = async () => {
   try {
-    await sequelize.authenticate();
+    await sequelize.authenticate({ logging: false });
     console.log("DB is authenticated");
   } catch (error) {
     console.log(error);
@@ -35,8 +35,8 @@ export const testDB = async () => {
 
 export const connectDB = async () => {
   try {
-    await sequelize.sync({ alter: true });
-    // console.log("DB connected");
+    await sequelize.sync({ alter: true, logging: false });
+    console.log("DB connected");
   } catch (error) {
     console.log(error);
   }
